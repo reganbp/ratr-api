37cf28a48b1883cb6fffb80878beb148491e0acd
reganbp <reganbp@gmail.com>
2018-08-07 10:15:25 -0400

    Add resource, Brewery
    - use scaffold to generate Brewery resource with columns name and
    location

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :breweries
   resources :ratings
   # RESTful routes
   resources :examples, except: %i[new edit]
---
+++ db/migrate/20180807141013_create_breweries.rb
@@ -0,0 +1,10 @@
+class CreateBreweries < ActiveRecord::Migration[5.1]
+  def change
+    create_table :breweries do |t|
+      t.string :name
+      t.string :location
+
+      t.timestamps
+    end
+  end
+end
