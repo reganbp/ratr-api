3832e4eb52dce774c0253d0413ee481ec685bb0f
reganbp <reganbp@gmail.com>
2018-08-07 10:19:46 -0400

    Add resource Brew
    - Use scaffold to generate resource Brew with column name

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :brews
   resources :breweries
   resources :ratings
   # RESTful routes
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180807141013) do
+ActiveRecord::Schema.define(version: 20180807141806) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,12 @@ ActiveRecord::Schema.define(version: 20180807141013) do
   create_table "breweries", force: :cascade do |t|
     t.string "name"
     t.string "location"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "brews", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
